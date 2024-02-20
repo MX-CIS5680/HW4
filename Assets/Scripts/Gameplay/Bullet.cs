a9478436be26070ab8acc02096cd05dd4b762300
Mengxuan <18082628d@connect.polyu.hk>
2024-02-19 21:51:04 -0500

    update UFO & Bullets

--- Assets/Scripts/Gameplay/Bullet.cs
+++ Assets/Scripts/Gameplay/Bullet.cs
@@ -11,17 +11,22 @@ namespace MyFirstARGame
         [SerializeField]
         private Material[] projectileMaterials;
 
+        public int PlayerCount;
+
         private void Awake()
         {
             // Pick a material based on our player number so that we can distinguish between projectiles. We use the player number
             // but wrap around if we have more players than materials. This number was passed to us when the projectile was instantiated.
             // See ProjectileLauncher.cs for more details.
-            var photonView = this.transform.GetComponent<PhotonView>();
-            var playerId = Mathf.Max((int)photonView.InstantiationData[0], 0);
-            if (this.projectileMaterials.Length > 0)
+            if(photonView.IsMine)
             {
-                var material = this.projectileMaterials[playerId % this.projectileMaterials.Length];
-                this.transform.GetComponent<Renderer>().material = material;
+                var photonView = this.transform.GetComponent<PhotonView>();
+                var playerId = Mathf.Max((int)photonView.InstantiationData[0], 0);
+                if (this.projectileMaterials.Length > 0)
+                {
+                    var material = this.projectileMaterials[playerId % this.projectileMaterials.Length];
+                    this.transform.GetComponent<Renderer>().material = material;
+                }
             }
         }
 
